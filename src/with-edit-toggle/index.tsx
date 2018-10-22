7d13528d4cf71a659bb53d3e7fadbe52b7db626c
brendanatme <registrations@brendanat.me>
2018-10-22 18:14:21 -0400

    Reference external typings in with-edit-toggle
    - we do this for publishing purposes

--- src/with-edit-toggle/index.tsx
+++ src/with-edit-toggle/index.tsx
@@ -27,6 +27,9 @@
 import * as React from 'react';
 import * as enhanceWithClickOutside from 'react-click-outside';
 
+type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
+type Subtract<T, K> = Omit<T, keyof K>;
+
 export interface IWithEditToggleOptions {
   clickToEdit: boolean;
   clickoff: boolean;
