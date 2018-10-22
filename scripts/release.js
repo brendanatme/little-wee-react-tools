9b3dfe182d4295de780e2e235c7b3a3fd5c3c9d8
brendanatme <registrations@brendanat.me>
2018-10-22 18:00:16 -0400

    Add release script

---
+++ scripts/release.js
@@ -0,0 +1,17 @@
+
+const { execSync } = require('child_process');
+
+const bump = process.argv[2];
+
+console.log('Releasing...');
+
+console.log('Running bundle...');
+execSync(`npm run bundle`, err => console.log(err));
+
+console.log(`Bumping version by: ${bump}...`);
+execSync(`npm version ${bump}`, err => console.log(err));
+
+console.log('Publishing to NPM...');
+execSync(`npm publish`, err => console.log(err));
+
+console.log('Success!');
