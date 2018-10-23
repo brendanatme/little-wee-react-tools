d24d3d6ecaf3ba5b1cbbaad22a4e8cc6978ed280
brendanatme <registrations@brendanat.me>
2018-10-23 12:04:06 -0400

    Try new publishing process

--- scripts/release.js
+++ scripts/release.js
@@ -8,11 +8,17 @@ console.log('Releasing...');
 console.log('Running bundle...');
 execSync(`npm run bundle`, err => console.log(err));
 
+console.log('Copying package.json...');
+execSync(`cp package.json dist/package.json`, err => console.log(err));
+
+console.log('Copying .npmignore...');
+execSync(`cp .npmignore dist/.npmignore`, err => console.log(err));
+
 console.log(`Bumping version by: ${bump}...`);
-execSync(`npm version ${bump}`, err => console.log(err));
+execSync(`cd dist; npm version ${bump}`, err => console.log(err));
 
 console.log('Publishing to NPM...');
-execSync(`npm publish`, err => console.log(err));
+execSync(`cd dist; npm publish`, err => console.log(err));
 
 console.log('Pushing to Git...');
 execSync(`git push`, err => console.log(err));
